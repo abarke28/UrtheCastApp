8f69aec43c29ac4c17866b3b699d63a2f837eea6
Alex Barker <abarke28@gmail.com>
2020-03-06 11:13:03 -0800

    Question 1. Started sequence with F0 = 0

--- UrtheCastApplication/Program.cs
+++ UrtheCastApplication/Program.cs
@@ -6,7 +6,7 @@ namespace UrtheCastApplication
     {
         static void Main(string[] args)
         {
-            for(int i=1; i<=10; i++)
+            for(int i=0; i<=10; i++)
             {
                 Console.WriteLine("Fibonacci number {0}: {1}", i, Fibonacci(i));
             }
@@ -14,13 +14,13 @@ namespace UrtheCastApplication
 
         public static int Fibonacci(int n)
         {
-            //Calculate nth Fibonacci via rounding
+            //Calculate nth Fibonacci via rounding. Considering F0 = 0
 
-            if (n <= 0) throw new ArgumentException("Argument must be integer > 0");
+            if (n < 0) throw new ArgumentException("Argument must be integer >= 0");
 
             double phi = (1 + Math.Sqrt(5)) / 2.0;
 
-            return Convert.ToInt32(Math.Pow(phi, Convert.ToDouble(n-1)) / Math.Sqrt(5));
+            return Convert.ToInt32(Math.Pow(phi, Convert.ToDouble(n)) / Math.Sqrt(5));
         }
     }
 }

c4164f71a84e0716aa55e1bf0b5ba2c3f47875c7
Alex Barker <abarke28@gmail.com>
2020-03-06 11:07:51 -0800

    Add project files.

---
+++ UrtheCastApplication/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace UrtheCastApplication
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            for(int i=1; i<=10; i++)
+            {
+                Console.WriteLine("Fibonacci number {0}: {1}", i, Fibonacci(i));
+            }
+        }
+
+        public static int Fibonacci(int n)
+        {
+            //Calculate nth Fibonacci via rounding
+
+            if (n <= 0) throw new ArgumentException("Argument must be integer > 0");
+
+            double phi = (1 + Math.Sqrt(5)) / 2.0;
+
+            return Convert.ToInt32(Math.Pow(phi, Convert.ToDouble(n-1)) / Math.Sqrt(5));
+        }
+    }
+}

a9046f7ad829e59d7c33cc2a053e9a233c184dde
Alex Barker <abarke28@gmail.com>
2020-03-06 11:57:22 -0800

    Added Question 2

--- UrtheCastApplication/Program.cs
+++ UrtheCastApplication/Program.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Linq;
 
 namespace UrtheCastApplication
 {
@@ -6,21 +8,38 @@ namespace UrtheCastApplication
     {
         static void Main(string[] args)
         {
-            for(int i=0; i<=10; i++)
+            Console.WriteLine("Question 1:");            
+            for (int i = 0; i <= 10; i++)
             {
                 Console.WriteLine("Fibonacci number {0}: {1}", i, Fibonacci(i));
+            }
+
+            Console.WriteLine("Question 2:");
+            var list = ListBuilderSorter(new List<string> { "one", "two", "three" }, new List<string> { "one", "two", "five", "six" }, new List<string> { "two", "five" });
+            
+            foreach(string s in list)
+            {
+                Console.WriteLine(s);
             }
         }
 
         public static int Fibonacci(int n)
         {
-            //Calculate nth Fibonacci via rounding. Considering F0 = 0
+            //Calculates nth Fibonacci via rounding. Considering F0 = 0
 
             if (n < 0) throw new ArgumentException("Argument must be integer >= 0");
 
             double phi = (1 + Math.Sqrt(5)) / 2.0;
 
             return Convert.ToInt32(Math.Pow(phi, Convert.ToDouble(n)) / Math.Sqrt(5));
+        }
+
+        public static List<string> ListBuilderSorter(IEnumerable<string> original, IEnumerable<string> add, IEnumerable<string> remove)
+        {
+            //Returns new list of strings that contains strings present in original, plus strings in add list, 
+            //minus strings in remove list, ordered by length then reverse alphabetically. Implemented with LINQ
+
+            return new List<string>(original).Concat(add).Distinct().Except(remove).OrderByDescending(s => s.Length).ThenByDescending(s => s).ToList();
         }
     }
 }
